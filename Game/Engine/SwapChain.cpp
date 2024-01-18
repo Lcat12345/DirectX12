014c2bb12690d2163bccb81fa4e5751b481b8e3d
Lcat <ljj7403@naver.com>
2024-01-18 15:23:29 +0900

    1차 시도
    omg

--- Game/Engine/SwapChain.cpp
+++ Game/Engine/SwapChain.cpp
@@ -1,3 +1,25 @@
 #include "pch.h"
 #include "SwapChain.h"
+#include "Device.h"
 
+void SwapChain::Init(const WindowInfo& info, ComPtr<Device> _device)
+{
+	// 혹시 모르니 리셋
+	_swapChain.Reset();
+
+	// 스왑 체인의 설명자 채우기
+	DXGI_SWAP_CHAIN_DESC _scd;
+	_scd.BufferCount = SWAP_CHAIN_BUFFER_COUNT;	// 버퍼의 개수는 두개
+	_scd.BufferDesc.Width = info.width;
+	_scd.BufferDesc.Height = info.height;
+	_scd.BufferDesc.RefreshRate; // 기억 안남
+	_scd.BufferDesc.Format = DXGI_FORMAT_R8G8_UINT; // 기억 안남
+	_scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED; // 기억 안남
+	_scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED; // 기억 안남
+	_scd.SampleDesc.Count = 60;
+	_scd.SampleDesc.Quality = 1;
+	
+	// 스왑 체인 만들기
+	_device->GetDXGI()->CreateSwapChain(_device.Get(), &_scd, IID_PPV_ARGS(&_swapChain));
+
+}
--- Game/Engine/SwapChain.h
+++ Game/Engine/SwapChain.h
@@ -1,11 +1,21 @@
 #pragma once
 
+// 
+// 외주를 맡김.
+// 외주 맡긴 걸 받아서 화면에 출력함.
+// 
+
 class SwapChain
 {
 public:
+	void Init(const WindowInfo& info, ComPtr<Device> _device);
 
 
 private:
+	ComPtr<IDXGISwapChain> _swapChain;
+	ComPtr<ID3D12Resource> _buffer;
+
+
 
 };
 
